76b40ab779979b34953554391e163774a3e598e7
Valentina2021 <u1201936@unimilitar.edu.co>
2020-09-15 21:53:55 -0500

    initial commit of the app

---
+++ src/components/tourData.js
@@ -0,0 +1,42 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Be ready to explore one of the biggest cities in the US! You won't regret it"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Vous n'avez pas choisir ton chemin? N'en doute pas! Venez à Paris"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Considering London as your next destination? Be aware there's a lot of excitement waiting for you"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Konichiwa! Visiting Tokyo will give you huge insights of its culture, their food and so on!"
+  },
+  {
+    id: 5,
+    city: "Montreal",
+    img: "./img/Montreal.jpg",
+    name: "Montreal, Canada",
+    info:
+      "Welcome to Montreal! It's a wonderful place to enjoy with your family and friends"
+  }
+];
